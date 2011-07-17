507d0d67ef977e0f6cac7d55e1e1fd8b0754a17a
brian d foy <brian.d.foy@gmail.com>
2011-07-16 21:25:00 -0500

    Clean up the documentation

--- lib/Net/SSH/Perl/ProxiedIPC.pm
+++ lib/Net/SSH/Perl/ProxiedIPC.pm
@@ -1,49 +1,65 @@
 package Net::SSH::Perl::ProxiedIPC;
-use strict; use warnings;
+use strict;
+use warnings;
 
 our $VERSION = '0.01';
 
 =head1 NAME
 
-Net::SSH::Perl::ProxiedIPC;
-
+Net::SSH::Perl::ProxiedIPC - Make long distance SSH commands
 
 =head1 SYNOPSIS
 
-  my $ssh = Net::SSH::Perl->new( ... );
+	my $ssh = Net::SSH::Perl->new( ... );
 
-  my $pipc = Net::SSH::Perl::ProxiedIPC->new( ssh => $ssh );
+	my $pipc = Net::SSH::Perl::ProxiedIPC->new( ssh => $ssh );
 
-  {
-    my ($cmd, $perlssh) = $pipc->open;
+	{
+	my ($cmd, $perlssh) = $pipc->open;
 
-    $perlssh->eval( "use POSIX qw(uname)" );
-    my @uname = $perlssh->eval( "uname()" ); # Returns the host of $ssh
-  }
+	$perlssh->eval( "use POSIX qw(uname)" );
+	my @uname = $perlssh->eval( "uname()" ); # Returns the host of $ssh
+	}
 
-  {
-    # Go from this host through host1 to host2
-    my ($cmd, $perlssh) = $pipc->open( 'user1@host1', 'user2@host2' );
+	{
+	# Go from this host through host1 to host2
+	my ($cmd, $perlssh) = $pipc->open( 'user1@host1', 'user2@host2' );
 
-    $perlssh->eval( "use POSIX qw(uname)" );
-    my @uname = $perlssh->eval( "uname()" ); # Returns 'host2'
-  }
+	$perlssh->eval( "use POSIX qw(uname)" );
+	my @uname = $perlssh->eval( "uname()" ); # Returns 'host2'
+	}
 
 =head1 DESCRIPTION
 
-This is a utility module that wraps around two SSH modules; L<Net::SSH::Perl>
-and L<IPC::PerlSSH>. By leveraging PerlSSH against the authenticated long-distance requests,
-you create a means to access data that would otherwise be secured and unaccessbile
-from the outside world, such as if you were on site with a client. And it lets you call Perl
-from the remote machine! Yay!
+This is a utility module that wraps around two SSH modules;
+L<Net::SSH::Perl> and L<IPC::PerlSSH>. By leveraging PerlSSH against
+the authenticated long-distance requests, you create a means to access
+data that would otherwise be secured and unaccessible from the outside
+world, such as if you were on site with a client. And it lets you call
+Perl from the remote machine! Yay!
+
+=head1 METHODS
+
+=over 4
+
+=item new
+
+Create a new proxied object by passing the object you
+create with C<Net::SSH::Perl>.
+
+=item open
+
+This is just a wrapper around C<_open_perlssh>, an internal function you
+shouldn't have to worry about.
+
+=back
 
 =cut
 
 use IPC::PerlSSH;
 
 sub new {
-  my $proto = shift;
-  my $class = ref $proto || $proto;
+  my( $class ) = shift;
   bless( { @_ }, $class );
 }
 
@@ -75,13 +91,15 @@ sub _open_perlssh {
   my $ssh = $self->_ssh;
 
   my $env_str = $self->_ssh_env_vars;
-  my $command = join ' ', (map "ssh -o StrictHostKeyChecking=no -A $_", @hosts), "perl";
-  $command = "sh -c '$env_str$command'"; 
+  my $command = join ' ',
+  	(map "ssh -o StrictHostKeyChecking=no -A $_", @hosts),
+  	"perl";
+  $command = "sh -c '$env_str$command'";
   my( $read, $write ) = $ssh->open2($command);
 
-  my $readfunc = sub { sysread( $read, $_[0], $_[1] ) };
+  my $readfunc  = sub { sysread( $read, $_[0], $_[1] ) };
   my $writefunc = sub { syswrite( $write, $_[0] ) };
-  
+
   ($command, IPC::PerlSSH->new( Readfunc => $readfunc, Writefunc => $writefunc ));
 }
 
@@ -92,10 +110,18 @@ sub open { shift->_open_perlssh( @_ ) }
 
 __END__
 
+=head1 SEE ALSO
+
+L<Net::SSH::Perl>
+
 =head1 AUTHOR
 
-Jennie Rose Evers-Corvina C<seven@nanabox.net>, Matthew S Trout
+Jennie Rose Evers-Corvina C<< <seven@nanabox.net> >>, Matthew S Trout
 
-Maintained by Jennie Rose Evers-Corvina. Please send patches, ideas or comments to C<withsocks@sevvie.co>.
+=head1 COPYRIGHT
+
+=head1 LICENSE
+
+You can use this package under the same terms as Perl itself.
 
 =cut
